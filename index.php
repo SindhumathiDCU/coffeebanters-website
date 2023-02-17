28d9751d6447e2f1b3442f1a8d34c7049bb1f113
SindhumathiDCU <125675881+SindhumathiDCU@users.noreply.github.com>
2023-02-17 17:20:11 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php header( 'Location: /Contact_us.html' ) ;  ?>
-<?php header( 'Location: /specials.html' ) ;  ?>
-<?php header( 'Location: /about_us.html' ) ;  ?>
+<?php include_once( 'Location: /Contact_us.html' ) ;  ?>
+<?php include_once( 'Location: /specials.html' ) ;  ?>
+<?php include_once( 'Location: /about_us.html' ) ;  ?>
 <?php header( 'Location: /index.html' ) ;  ?>
